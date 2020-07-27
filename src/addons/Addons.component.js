0acd5d325f87512e3d114b336e7818c6048100b3
Shriramoji <rshriramoji@deloitte.com>
2020-07-27 16:57:20 +0530

    added addons fields
    added addons fields

--- src/addons/Addons.component.js
+++ src/addons/Addons.component.js
@@ -7,12 +7,49 @@ export class AddonsComponent extends React.Component {
     constructor(props){
         super(props);
         this.state ={
-          addonsList:[]
+          addonsList:[],
+          productDetails: {
+            addons: []
+          }
         }
     }
 
     componentDidMount(){
-      this.setState({addonsList:JSON.parse(sessionStorage.getItem('metadata'))[0].addons});
+      this.setState({addonsList:JSON.parse(sessionStorage.getItem('metadata'))[0].addons},function(){
+        this.prodDetails = commonService.getProductdetails();
+        let values = this.state.addonsList.filter(list=>{
+          let isAvailable = false;
+          list.applications.forEach(app=>{
+            if(this.prodDetails.applications.indexOf(app)>-1){
+              isAvailable = true;
+            }
+          });
+          return isAvailable;
+        });
+        console.log(values);
+        this.setState({addonsList:values});
+        console.log(this.state.addonsList);
+      });
+      this.prodDetails = commonService.getProductdetails();
+      this.prodDetails.addons.forEach(app=>{
+        this.state.productDetails.addons.push(app);
+      });
+      this.setState({productDetails: this.state.productDetails});
+    }
+
+    componentWillUnmount() {
+      commonService.saveProductdetails(this.state.productDetails.addons,'addons');
+    }
+
+    saveItem=(item)=>{
+      const index = this.state.productDetails.addons.indexOf(item);
+      if(index===-1){
+        this.state.productDetails.addons.push(item);
+      } else {
+        this.state.productDetails.addons.splice(index,1);
+      }
+      this.setState({productDetails: this.state.productDetails});
+      console.log(this.state.productDetails.addons);
     }
 
     render() {
@@ -22,7 +59,7 @@ export class AddonsComponent extends React.Component {
             <div className="items">
               {this.state.addonsList.map((value,i) => (
                 <div className="col-md-6"  key={i}>
-                  <button className="button button1">
+                  <button className="button button1" className={this.state.productDetails.addons.indexOf(value.code)>-1? 'is-active' : 'deactive'} onClick={()=>{this.saveItem(value.code)}}>
                   <div className="icon">
                   <img className="buttonIcon" src={iconsBaseUrl+value.icon}/>
                   </div>{value.name}</button>
